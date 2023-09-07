3ad95607b8d4edd6543cf1355a559de3e9b492ef
rajeshjanapati <rajeshjanapati@gmail.com>
2023-09-07 17:46:58 +0530

    33

--- scripts.ps1
+++ scripts.ps1
@@ -16,7 +16,7 @@ $githubUsername = "rajeshjanapati"
 $sourceRepo = "apigee-encrypt-keys"
 $branchName = "encrypt/keys"
 $filePath = "jsonfiles/base64_encoded_app.json"
-$pat = "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"
+$pat = "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"
 
 # Define the GitHub API URL for fetching the file content from a specific branch
 $apiUrl = "https://api.github.com/repos/$githubUsername/$sourceRepo/contents/$filePath?ref=$branchName"
