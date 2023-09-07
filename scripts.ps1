3087f213205b5c5aac9a321e135b2ea605dafd02
rajeshjanapati <rajeshjanapati@gmail.com>
2023-09-07 16:41:29 +0530

    22

--- scripts.ps1
+++ scripts.ps1
@@ -10,23 +10,48 @@ $repositoryName = "apigee-encrypt-keys"
 $branchName = "encrypt/keys"  # Change this to the branch you want to access
 $githubToken = "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"
 
-# # Clone the repository
-git clone https://github.com/rajeshjanapati/apigee-encrypt-keys.git
-# cd $repositoryName
 
-cd jsonfiles
+# Define your GitHub username, repository names, branch name, and file path
+$githubUsername = "rajeshjanapati"
+$sourceRepo = "apigee-encrypt-keys"
+$branchName = "encrypt/keys"
+$filePath = "jsonfiles/base64_encoded_app.json"
+$pat = "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"
 
-# # Use Get-ChildItem to list all files in the directory with a .zip extension
-$zipFiles = Get-ChildItem -Path $directoryPath -Filter *.zip
+# Define the GitHub API URL for fetching the file content from a specific branch
+$apiUrl = "https://api.github.com/repos/$githubUsername/$sourceRepo/contents/$filePath?ref=$branchName"
 
-# Read JSON files
-$jsonFiles = Get-ChildItem -Filter *.json -Recurse
-
-# Loop through each JSON file and make POST requests
-foreach ($jsonFile in $jsonFiles) {
-    $jsonContent = Get-Content -Path $jsonFile -Raw
-    # Parse the JSON content
-    $jsonData = ConvertFrom-Json $jsonContent
-
-    Write-Host $jsonData
+# Set the request headers with your PAT
+$headers = @{
+    Authorization = "Bearer $pat"
 }
+
+# Make a GET request to fetch the file content
+$fileContent = Invoke-RestMethod -Uri $apiUrl -Headers $headers
+
+# Parse and display the file content (in this case, it's assumed to be JSON)
+$jsonContent = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($fileContent.content))
+Write-Host "JSON File Content:"
+Write-Host $jsonContent
+
+
+# # # Clone the repository
+# git clone https://github.com/rajeshjanapati/apigee-encrypt-keys.git
+# # cd $repositoryName
+
+# cd jsonfiles
+
+# # # Use Get-ChildItem to list all files in the directory with a .zip extension
+# $zipFiles = Get-ChildItem -Path $directoryPath -Filter *.zip
+
+# # Read JSON files
+# $jsonFiles = Get-ChildItem -Filter *.json -Recurse
+
+# # Loop through each JSON file and make POST requests
+# foreach ($jsonFile in $jsonFiles) {
+#     $jsonContent = Get-Content -Path $jsonFile -Raw
+#     # Parse the JSON content
+#     $jsonData = ConvertFrom-Json $jsonContent
+
+#     Write-Host $jsonData
+# }
